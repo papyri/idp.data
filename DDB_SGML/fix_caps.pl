132b0715a9ab0a79a597940dcf69fbf27da1b6a8
Hugh A. Cayless <hcayless@mid-campus-03418.wireless.unc.edu>
2010-03-18 14:20:47 -0400

    Removing legacy data.

--- DDB_SGML/fix_caps.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-# fix beta-code capital letters:  accents and breathings before the letter, not after
-
-while(<>)
-{
-    s|\*([a-z])([()/\\=\|]+)|*$2$1|gi;
-#    s|\*([aehowu])([()/\\=\|]+)([^aehiowu])|\*$2$1$3|gi;
-    print;
-}
